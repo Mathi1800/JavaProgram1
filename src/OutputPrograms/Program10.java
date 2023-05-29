854319c574a8467af655fe7b6e0625ece5eeddb3
Mathivanan <mathivanan.sakthivel10@gmail.com>
2023-05-29 17:27:44 +0530

    Area of the circle

--- src/OutputPrograms/Program10.java
+++ src/OutputPrograms/Program10.java
@@ -1,9 +1,22 @@
+//Program :10
+//Find the Area of the circle 
+
 package OutputPrograms;
+
+import java.util.Scanner;
 
 public class Program10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Enter the radius:");
+		Scanner scanner = new Scanner(System.in);
+		double radius = scanner.nextDouble();
+		scanner.close();
+
+		// Math.pi is the function of pi
+
+		System.out.println("Area of the circle =" + (Math.PI * radius * radius));
 
 	}
 
