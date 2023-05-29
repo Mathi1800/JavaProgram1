eb0ac9c0f1ea185b628f400f067bca198a7f4cc8
Mathivanan <mathivanan.sakthivel10@gmail.com>
2023-05-29 18:20:22 +0530

    Sum of 100:

---
+++ src/OutputPrograms/Program14.java
@@ -0,0 +1,19 @@
+//Program : 14
+//program for sum of 100
+
+package OutputPrograms;
+
+public class Program14 {
+
+	public static void main(String[] args) {
+//Initializing the result 
+		int result = 0;
+		for (int i = 1; i < 100; i++) {
+
+			result = result + i;
+
+		}
+		System.out.println("The sum of 100 =" + result);
+	}
+
+}
